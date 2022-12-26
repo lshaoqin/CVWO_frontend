b0452a209aaa57d702618669ac3a841c59fc494b
lshaoqin <shaoqin.lu@gmail.com>
2022-12-26 21:50:41 +0800

    Added template for post component

---
+++ src/components/posts/PostCard.tsx
@@ -0,0 +1,45 @@
+{/* Template for each post in Thread view. */}
+
+import Post from '../../types/Post';
+import React from 'react';
+import { Card, CardContent, makeStyles, Typography } from '@material-ui/core';
+
+type Props = {
+    post: Post;
+};
+
+const useStyles = makeStyles({
+    titleBody: {
+        fontSize: 20,
+        whiteSpace: 'pre-wrap',
+        paddingBottom: '1em',
+    },
+    postCard: {
+        marginBottom: '1em',
+    },
+    metadata: {
+        fontSize: 12,
+    },
+});
+
+const PostDisplayItem: React.FC<Props> = ({ post }) => {
+    const classes = useStyles();
+
+    return (
+        <Card className={classes.titleBody}>
+            <CardContent>
+                <Typography variant="body2" color="textPrimary" className={classes.titleBody} component="p">
+                    {post.title}
+                </Typography>
+                <Typography color="textSecondary" className={classes.metadata} gutterBottom>
+                    {'Posted by ' + post.author + ' on ' + post.timestamp.toLocaleString()}
+                </Typography>
+            </CardContent>
+        </Card>
+    );
+  
+
+
+};
+
+export default PostDisplayItem;
---
+++ src/types/Post.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+type Post = {
+    title: string;
+    body: string;
+    author: string;
+    timestamp: Date;
+};
+
+export default Post;
