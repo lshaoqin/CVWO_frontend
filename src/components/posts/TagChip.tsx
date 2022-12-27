c4dcab90957e9a0d1444ee16479fd5297c1f2328
lshaoqin <shaoqin.lu@gmail.com>
2022-12-27 14:52:21 +0800

    Added component for tags

---
+++ src/components/posts/TagChip.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Chip } from '@material-ui/core';
+import Tag from '../../types/Tag';
+
+type Props = {
+    tag: Tag;
+};
+
+const TagChip: React.FC<Props> = ({tag}) => {
+  return (
+    <Chip
+      label={tag.name + ' ' + tag.weight}
+      onClick={() => console.log('Chip clicked') //TODO: Increase/decrease tag weight on click
+    }
+      clickable
+      color="secondary" //TODO: Change tag colour based on vote
+      variant="outlined"
+    />
+  );
+}
+
+export default TagChip;
