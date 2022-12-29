67530cf451ffc80123dcb6f6a5efc910128c93c3
lshaoqin <shaoqin.lu@gmail.com>
2022-12-29 20:21:15 +0800

    added axios and get/post requests

---
+++ src/services/request.tsx
@@ -0,0 +1,27 @@
+import axios from 'axios'
+
+const API_URL: string = 'http://localhost:3000'
+
+async function getRequest(params: object): Promise<object> {
+    try 
+    {
+        const response = await axios.get(API_URL, { params });
+        return response.data;
+    } 
+    catch (error: any) 
+    {
+        throw new Error(`Failed to get data from API: ${error.message}`);
+    }
+}
+
+async function postRequest(params: object): Promise<object> {
+    try 
+    {
+        const response = await axios.post(API_URL, { params });
+        return response.data;
+    } 
+    catch (error: any) 
+    {
+        throw new Error(`Failed to post data to API: ${error.message}`);
+    }
+}
