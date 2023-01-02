36e2e5b1df5dcf422b498eb49d19ecae14bac804
lshaoqin <shaoqin.lu@gmail.com>
2023-01-02 11:21:18 +0800

    Added constraints to TagSelector

--- src/components/posts/TagSelector.tsx
+++ src/components/posts/TagSelector.tsx
@@ -8,23 +8,35 @@ import Grid from '@mui/material/Grid';
 
 const TagSelector: React.FC = () => {
     const [tags, setTags] = React.useState<Array<string>>([]);
+    const [value, setValue] = React.useState<string>('');
 
     const handleRemove = (tagToRemove: string) => {
         setTags(tags.filter(tag => tag !== tagToRemove));
       };
     
     return (
-        <Container component="main" maxWidth="md">
+        <Container component="main" maxWidth="lg">
+            <Grid container spacing={2}>
             <Grid item xs={4}>
             <Autocomplete
             freeSolo
                 options={tagOptions}
                 renderInput={(params) => 
-                    <TextField {...params} label="Search for text" variant="outlined" />
+                    <TextField {...params} 
+                    label="Enter tags here" 
+                    value={value}
+                    variant="outlined" 
+                    inputProps={{
+                        maxLength: 16,
+                      }}/>
                 }
                 onChange={(event: any, newValue: string | null) => {
                     if (newValue){
+                        //Max number of tags is 20, and doesn't include duplicate tags.
+                        if (tags.length<20 && !tags.includes(newValue)) {
                         setTags([...tags, newValue]);
+                        setValue('');
+                        }
                     }
                   }}
             />
@@ -35,12 +47,12 @@ const TagSelector: React.FC = () => {
                         label={tag}
                         onClick={() => handleRemove(tag)}
                         clickable
-                        color="primary" 
+                        color="secondary" 
                         variant="outlined"
                     />
                   ))}
             </Grid>
-
+            </Grid>        
         </Container>
     );
 }
