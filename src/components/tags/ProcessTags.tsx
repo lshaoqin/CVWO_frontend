3a67bd0f97e822519375d919fa14b7caf520badc
lshaoqin <shaoqin.lu@gmail.com>
2023-01-04 11:53:39 +0800

    drafted ProcessTags function

--- src/components/tags/ProcessTags.tsx
+++ src/components/tags/ProcessTags.tsx
@@ -1,7 +1,45 @@
 import Tag from '../../types/Tag'
 
-type TagEntry {
-    
+type TagEntry = {
+    name: string,
+    weight: number,
+    post_id: number,
+    user_id: number,
 }
 
-function ProcessTags(tagEntries)
+interface Counter {
+    [name: string]: [number, number]
+}
+
+function ProcessTags(tagEntries: TagEntry[]) {
+    const tagcounts = tagEntries.reduce((counters: Counter, {name, weight, user_id}: TagEntry) => {
+        if(user_id == 0) {
+            if (counters[name]) {
+                counters[name][0] += weight
+            }
+            else {
+                counters[name] = [weight, 0]
+            }
+        }
+
+        if(user_id == 1) {
+            if (counters[name]) {
+                counters[name][1] += weight
+            }
+            else {
+                counters[name] = [0, weight]
+            }
+        }
+
+        return counters
+    }, {})
+
+    const tags: Tag[] = []
+    for (const name in tagcounts) {
+        tags.push({name: name, weight: tagcounts[name][0], userWeight: tagcounts[name][1]})
+    }
+    
+    return tags;
+}
+
+export default ProcessTags
