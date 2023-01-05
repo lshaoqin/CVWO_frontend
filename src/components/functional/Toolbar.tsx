45c078566b140cba4d2440efa38f3884b4972555
lshaoqin <shaoqin.lu@gmail.com>
2023-01-05 22:27:13 +0800

    improved toolbar design

--- src/components/functional/Toolbar.tsx
+++ src/components/functional/Toolbar.tsx
@@ -10,15 +10,28 @@ const tb: React.FC<ToolbarProps> = ({ label }) => {
 
     return (
         <AppBar position="static">
-        <Toolbar>
+        <Toolbar style={{ display: "flex", justifyContent: "space-between" }}>
         <div style={{ display:'flex', justifyContent: 'left'}}>
-            <Typography variant="h6" color="inherit" component="div">
-                {label}
-            </Typography>
+            
+                <Link to="/" style={{ textDecoration: 'none', color: 'white' }}>
+                    <Button color="inherit">
+                    <Typography variant="h5" color="inherit" component="div" style={{ textTransform: 'none' }}>
+                    {label}
+                    </Typography>
+                    </Button>
+                </Link>
+
         </div>
+        <div style={{ display:'flex'}}>
+        <Typography variant="h6" color="inherit" component="div">
+            Logged in as {localStorage.getItem('username')} - 
             <Link to="/logout" style={{ textDecoration: 'none', color: 'white' }}>
-                <Button color="inherit">Logout</Button>
+                <Button color="inherit">
+                <Typography variant="h6" color="inherit" component="div">Logout</Typography>
+                </Button>
             </Link>
+        </Typography>
+        </div>
         </Toolbar>
         </AppBar>
     )
